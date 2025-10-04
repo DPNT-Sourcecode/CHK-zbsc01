e259a1c6b0019ce4b30076eedd613ed2347210c2
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-04 18:34:27 +0000

    Sat Oct 04 18:34:27 UTC 2025

--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -6,7 +6,13 @@ import io.accelerate.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        if(x >= 0 && x <=100 && y >= 0 && y <= 100){
+            return x+y;
+        } else {
+            throw new IllegalArgumentException("the parameters are not satisfying conditions" +
+                    "x and y to be between 0 and 100 ");
+        }
     }
 
 }
+
