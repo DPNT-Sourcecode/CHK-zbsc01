1b8ace209417a84e8069638095c44b6536d1803b
sbx_user1051 <sbx_user1051@169.254.2.213>
2025-10-04 19:08:37 +0000

    Sat Oct 04 19:08:37 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,54 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        if (skus == null || skus.isEmpty()) {
+            return -1;
+        }
+
+        Map<Character, Integer> priceMap = new HashMap<>();
+        priceMap.put('A', 50);
+        priceMap.put('B', 30);
+        priceMap.put('C', 20);
+        priceMap.put('D', 15);
+
+        Map<Character, int[]> offerMap = new HashMap<>();
+        offerMap.put('A', new int[]{3, 130});
+        offerMap.put('B', new int[]{2, 45});
+
+        for (char item : skus.toCharArray()) {
+            if (!priceMap.containsKey(item)) {
+                return -1;
+            }
+        }
+        Map<Character, Integer> countMap = new HashMap<>();
+        for (char item : skus.toCharArray()) {
+            countMap.put(item, countMap.getOrDefault(item, 0) + 1);
+        }
+        int totalCost = 0;
+        for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+            char item = entry.getKey();
+            int count = entry.getValue();
+
+            if (offerMap.containsKey(item)) {
+                int[] offer = offerMap.get(item);
+                int offerQuantity = offer[0];
+                int offerPrice = offer[1];
+
+                int offerCount = count / offerQuantity;
+                int remainder = count % offerQuantity;
+
+                totalCost += offerCount * offerPrice;
+                totalCost += remainder * priceMap.get(item);
+
+            } else {
+                totalCost += count * priceMap.get(item);
+            }
+        }
+        return totalCost;
     }
 }
